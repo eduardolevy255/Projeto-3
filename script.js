3fcac412e16dc82a87d679aaa2dd75fdf21497e6
Eduardo <eduardovicentinilevy@gmail.com>
2024-04-04 09:21:40 -0300

    Criando o script de calcular

---
+++ script.js
@@ -0,0 +1,6 @@
+function Calcular(){
+    //DECLARANDO Variaveis
+    let num1=parseFloat(document.getElementById("num1").value);
+    let num2=parseFloat(document.getElementById("num2").value);
+    document.getElementById("Resultado").innerHTML =num1 * num2;
+}
